4df7c9d7539212b152976569ffde3cdffcd6a191
Artem <a.vasilchikov@aimprosoft.com>
2018-06-06 10:20:36 +0300

    Added Device file

---
+++ AimTestPod/AimTestPod/AimTestPod.h
@@ -0,0 +1,19 @@
+//
+//  AimTestPod.h
+//  AimTestPod
+//
+//  Created by Artem on 6/6/18.
+//  Copyright © 2018 Artem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AimTestPod.
+FOUNDATION_EXPORT double AimTestPodVersionNumber;
+
+//! Project version string for AimTestPod.
+FOUNDATION_EXPORT const unsigned char AimTestPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AimTestPod/PublicHeader.h>
+
+
